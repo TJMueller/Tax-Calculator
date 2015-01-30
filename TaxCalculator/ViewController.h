cc06f3947116253e6f5d3e86e9aee2658fc57be8
snowboarder56438 <timothymueller@Timothys-MacBook-Pro.local>
2015-01-30 13:04:52 -0600

    Initial Commit

---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Timothy Mueller on 1/30/15.
+//  Copyright (c) 2015 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaxCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaxCalculator
+//
+//  Created by Timothy Mueller on 1/30/15.
+//  Copyright (c) 2015 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
