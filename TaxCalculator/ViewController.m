e0282673cae8b98a82af2f3c2aceed16f298af72
snowboarder56438 <timothymueller@Timothys-MacBook-Pro.local>
2015-02-07 13:32:24 -0600

    First Commit

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 
 @end
 
@@ -16,12 +22,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+- (IBAction)onCalculateButtonTapped:(id)sender {
+    
+    NSString *enteredText = self.priceTextField.text;
+    double enteredValue =enteredText.intValue;
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0)
+    {
+        double result = enteredValue * self.caTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    
+    else if (self.segmentedControl.selectedSegmentIndex == 1)
+    {
+        double result = enteredValue * self.chiTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    
+    else
+    {
+        double result = enteredValue * self.nyTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
 }
 
 @end
+
+
+
--- TaxCalculator/main.m
+++ TaxCalculator/main.m
@@ -3,7 +3,8 @@
 //  TaxCalculator
 //
 //  Created by Timothy Mueller on 1/30/15.
-//  Copyright (c) 2015 TJ. All rights reserved.
+//  Copyright (c) 2015 TJ. All rights reserved.#12	0x0000000105e67893 in main at /Users/timothymueller/Desktop/iOS/TaxCalculator/TaxCalculator/main.m:14
+
 //
 
 #import <UIKit/UIKit.h>
